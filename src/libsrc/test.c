000f2c0d4250803b07c892966fbc613705396837
jiawenshi <jiawshi@126.com>
2026-06-06 13:30:28 +0800

    update

--- src/libsrc/test.c
+++ src/libsrc/test.c
@@ -14,7 +14,7 @@ int test_printf(void)
     printf("Hello, World!\n");
     printf("Hello, World!\n");
     printf("Hello, World!\n");
-    printf("Hello, World!111\n");
+    printf("Hello, World!11122222\n");
     return 0;
 }
 
