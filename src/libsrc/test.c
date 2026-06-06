131cffb4c8dcb24b59980279e4d5baa9df0596bf
jiawenshi <jiawshi@126.com>
2026-06-06 11:13:08 +0800

    update

--- src/libsrc/test.c
+++ src/libsrc/test.c
@@ -2,13 +2,13 @@
 
 int test_printf(void)
 {
+    printf("Hello, World\n");
     printf("Hello, World!\n");
-    printf("Hello, World2!\n");
-    printf("Hello, World3!\n");
-    printf("Hello, World4!\n");
-    printf("Hello, World5!\n");
-    printf("Hello, World6!\n");
-    printf("Hello, World7!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
     return 0;
 }
 
